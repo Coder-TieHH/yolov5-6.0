88041cda1009bb240cfc748104b298ad908cc7a3
root <wangjianhao200008@163.com>
2024-11-07 11:00:40 +0800

    A100-model1

--- val.py
+++ val.py
@@ -300,7 +300,7 @@ def run(data,
 def parse_opt():
     parser = argparse.ArgumentParser()
     parser.add_argument('--data', type=str, default=ROOT / 'data/VisDrone.yaml', help='dataset.yaml path')
-    parser.add_argument('--weights', nargs='+', type=str, default='/root/yolov5-6.0/runs/model_P4_dh/exp/weights/best.pt', help='model.pt path(s)')
+    parser.add_argument('--weights', nargs='+', type=str, default='/root/yolov5-6.0/runs/model_model1_A100/exp/weights/best.pt', help='model.pt path(s)')
     parser.add_argument('--batch-size', type=int, default=128, help='batch size')
     parser.add_argument('--imgsz', '--img', '--img-size', type=int, default=640, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.001, help='confidence threshold')
@@ -314,7 +314,7 @@ def parse_opt():
     parser.add_argument('--save-hybrid', action='store_true', help='save label+prediction hybrid results to *.txt')
     parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
     parser.add_argument('--save-json', action='store_true', help='save a COCO-JSON results file')
-    parser.add_argument('--project', default='/root/yolov5-6.0/runs/model_P4_dh', help='save to project/name')
+    parser.add_argument('--project', default='/root/yolov5-6.0/runs/model_model1_A100/exp', help='save to project/name')
     parser.add_argument('--name', default='exp', help='save to project/name')
     parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
     parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
